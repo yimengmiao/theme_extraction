1d248fc6dba1d69129599892677b36015d5d9108
luoruijie <luoruijie@163.com>
2024-09-03 15:32:51 +0800

    添加evaluate.py代码，主要是为了计算老师四分类任务中每个类别的precision,recall,f1和总的accuracy,

---
+++ evaluate.py
@@ -0,0 +1,109 @@
+from tqdm import tqdm
+import json
+import pandas as pd
+import re
+from difflib import SequenceMatcher
+from sklearn.metrics import classification_report
+
+df = pd.read_excel("data/726四分类法.xlsx")
+cloumn = "gpt4"
+
+
+def similarity(sent1, sent2):
+    return SequenceMatcher(None, sent1, sent2).ratio()
+
+
+def split_sentences(data):
+    split_data = []
+    for item in data:
+        # 使用正则表达式分割句子
+        sentences = re.split(r'[？。]', item['content'])
+        for sentence in sentences:
+            if sentence:  # 忽略空字符串
+                split_data.append({'type': item['type'], 'content': sentence})
+    return split_data
+
+
+gpt4o_output = []
+glm4_output = []
+manual_label = []
+for i in tqdm(range(0, len(df))):
+    # print(i)
+    manual = json.loads(df.loc[i, 'label'])
+    split_data_manual = split_sentences(manual)
+    if cloumn == 'gpt4':
+        gpt4o = json.loads(df.loc[i, 'gpt4o_predict']).get("result")
+        split_data_gpt4o_output = split_sentences(gpt4o)
+        for item in split_data_gpt4o_output:
+            for sent in split_data_manual:
+
+                if similarity(item.get("content"), sent.get("content")) > 0.5:
+                    item['label'] = sent.get("type")
+                    item['manual_content'] = sent.get("content")
+                    break  # 如果匹配上了，就不要再往下匹配了。
+
+        gpt4o_output.extend(split_data_gpt4o_output)
+
+    elif cloumn == 'glm4':
+        glm4 = json.loads(df.loc[i, 'glm4_predict'])
+        split_data_glm4_output = split_sentences(glm4)
+
+        for item in split_data_glm4_output:
+            for sent in split_data_manual:
+
+                if similarity(item.get("content"), sent.get("content")) > 0.5:
+                    item['label'] = sent.get("type")
+                    item['manual_content'] = sent.get("content")
+                    break  # 如果匹配上了，就不要再往下匹配了。
+
+        glm4_output.extend(split_data_glm4_output)
+
+# 定义一个包含四个必要键值的列表
+required_keys = ['type', 'content', 'label', 'manual_content']
+
+# 遍历每个字典
+# data = [
+#     {'type': '讲解', 'content': '猴子呀他一开始很勤劳啊，还是种了一棵梨树', 'label': '讲解', 'manual_content': '猴子呀他一开始很勤劳啊，还是种了一棵梨树'},
+#     {'type': '讲解', 'content': '可是当乌鸦来劝说它的时候，它就拔掉了梨树，改种了', 'label': '讲解', 'manual_content': '可是当乌鸦来劝说它的时候，它就拔掉了梨树，改种了'},
+#     {'type': '讲解', 'content': '正当杏树成活的时候，喜鹊来劝他了', 'label': '讲解', 'manual_content': '正当杏树成活的时候，喜鹊来劝他了'}
+# ]
+
+# 遍历列表并确保每个字典都有四个键值
+if cloumn == 'gpt4':
+    for item in gpt4o_output:
+        for key in required_keys:
+            if key not in item:
+                item[key] = ''  # 如果缺少键，就添加并赋值为空字符串
+    # 将数据转换为DataFrame
+    df = pd.DataFrame(gpt4o_output)
+
+    # 去除df_cleaned 中任意列值为 '' 的行
+    df = df[(df != '').all(axis=1)]
+    df_cleaned = df.dropna(subset='label').reset_index(drop=True)
+
+    df_cleaned['type'] = df_cleaned['type'].replace("其他", "其它")
+    df_cleaned['label'] = df_cleaned['label'].replace("其他", "其它")
+    df_cleaned.to_excel("data/gpt4o_T_4_result.xlsx", index=False)
+    # 计算分类报告
+    classification_rep = classification_report(df_cleaned['label'], df_cleaned['type'], zero_division=0)
+
+    print(classification_rep)
+
+if cloumn == 'glm4':
+    for item in glm4_output:
+        for key in required_keys:
+            if key not in item:
+                item[key] = ''  # 如果缺少键，就添加并赋值为空字符串
+    # 将数据转换为DataFrame
+    df = pd.DataFrame(glm4_output)
+
+    # 去除df_cleaned 中任意列值为 '' 的行
+    df = df[(df != '').all(axis=1)]
+    df_cleaned = df.dropna(subset='label').reset_index(drop=True)
+
+    df_cleaned['type'] = df_cleaned['type'].replace("其他", "其它")
+    df_cleaned['label'] = df_cleaned['label'].replace("其他", "其它")
+    df_cleaned.to_excel("data/glm4_T_4_result.xlsx", index=False)
+    # 计算分类报告
+    classification_rep = classification_report(df_cleaned['label'], df_cleaned['type'], zero_division=0)
+    print(classification_rep)
---
+++ glm4_api.py
@@ -0,0 +1,43 @@
+import json
+import re
+
+import pandas as pd
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="08bd304ed5c588b2c9cb534405241f0e.jPN6gjmvlBe2q1ZZ",
+    base_url="https://open.bigmodel.cn/api/paas/v4/"
+)
+prompt = """后面的“待分析文本”是一段师生对话，其中，学生话语已经剔除，只保留老师话语，请对老师的话语进行分析，具体分析方法如下所示：  
+将”待分析文本“分割成”发起“、”评价“、”讲解“、“其它”四种子文本段，”发起“的分割尽可能细一点。“发起”是老师邀请、引导、鼓励学生用话语来回应的语句；“评价”是对学生回应的表扬、认可、批评等评价性话语；”讲解“是老师针对知识展开描述或对学生回应的总结；不能归属于上面三种子文本段，归属为“其它”。
+按照下面“示例”输出： 
+示例： 老师话语： 它分别种了什么树呢？谁来说说？于凯，你来说说看。你慢讲啊，嗯。然后呢？ 
+输出： 
+[{"type":"发起"，"content":"它分别种了什么树呢？谁来说说？"} 
+{"type":"发起"，"content":"于凯，你来说说看。"} 
+{"type":"其它"："content":"你慢讲啊，嗯。"}
+{"type":"发起"："content":"然后呢？"}]
+待分析文本："""
+df = pd.read_excel("726四分类法.xlsx")
+for i in range(len(df)):
+    response = client.chat.completions.create(
+        model="glm-4",  # 使用的模型
+        response_format={"type": "json_object"},  # 响应格式为JSON对象
+        messages=[
+            {"role": "system", "content": "你是一个乐于助人的小助手,并且输出是一个json，key是result"},
+            {"role": "user", "content": prompt + df.loc[i, 'text']}
+        ],
+        max_tokens=1000,  # 最大生成的token数
+        n=1,  # 生成的结果数量
+        stop=None,  # 停止生成的标记
+        temperature=0.7,  # 生成文本的多样性
+    )
+    print(response.choices[0].message.content)
+    output_str = response.choices[0].message.content
+    # 提取json
+    matches = re.findall(r'\[.*\]', output_str, re.DOTALL)
+    df.loc[i,'glm4_api'] = matches[0]
+
+    # output_json = json.loads(matches[0])
+
+df.to_excel("726四分类法.xlsx",index=False)
